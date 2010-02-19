88adada5ee907419cfe282ac744a98813731950f
Ben Zuffi <bzuffi@gmail.com>
2010-02-19 14:34:16 -0800

    Plugin Rename

---
+++ gowpcms/index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Green Options Contract Management System
+Plugin URI: http://sites.google.com/site/gowpcms
+Description: A Wordpress Plugin that provides Contract Management functionalities
+Version: 0.1
+Author: The Spring 2010 SJSU MIS 119A Project Team
+Author URI: http://sjsumis.org/
+License: GPL2
+*/
+?>
+
+<?php
+/*  Copyright 2010 The Spring 2010 SJSU MIS 119A Project Team (email : bus119avirgance@groups.google.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
